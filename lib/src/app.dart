4a0d7803e61cad4739dbc524acc3bd0a6825c663
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-02-06 12:35:33 -0300

    Inicialização do app Modularizada

---
+++ lib/src/app.dart
@@ -0,0 +1,20 @@
+import 'package:Brisacliente/src/pages/LoginPage.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Brisacliente',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        backgroundColor: Color(0xFFecf0f1),
+        body: LoginPage(),
+      )
+    );
+  }
+}
