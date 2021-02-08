565502fd562047ea75f74c0798195b469f3e1f08
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-02-07 23:36:07 -0300

    Client Model criado

--- lib/src/models/client.dart
+++ lib/src/models/client.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 part 'client.g.dart';
 
@@ -12,5 +13,18 @@ abstract class _ClientBase with Store {
   @observable
   String cpf;
   @action
-  changeCpf(String value) => cpf = value;
+  changeCpf(String value){
+    if (cpf != null){
+      if (cpf.length == 2 && value.length == 3 ||
+          cpf.length == 6 && value.length == 7){
+        cpf = value + ".";
+      }else if (cpf.length == 10 && value.length == 11){
+        cpf = value + "-";
+      }else {
+        cpf = value;
+      }
+    }else {
+      cpf = value;
+    }
+  }
 }
