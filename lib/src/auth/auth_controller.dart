921cc6437ade1a48d87efe3f58196bcf1741e560
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-02-13 23:04:52 -0300

    Auth Controller implementado

--- lib/src/auth/auth_controller.dart
+++ lib/src/auth/auth_controller.dart
@@ -1,8 +1,28 @@
+import 'package:Brisacliente/src/auth/repositories/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mobx/mobx.dart';
 part 'auth_controller.g.dart';
 
 class AuthController = _AuthControllerBase with _$AuthController;
 
 abstract class _AuthControllerBase with Store {
+  var _authRepository = AuthRepository();
+
+  @observable
+  User user;
+
+  @action
+  setUser(User value) => user = value;
+
+  _AuthControllerBase(){
+    _authRepository.getUser().then(setUser);
+  }
   
+  Future loginWithEmailandPassword(email, password) async {
+    user = await _authRepository.getEmailPasswordLogin(email, password);
+  }
+
+  Future logout() async {
+    return _authRepository.getLogout();
+  }
 }
