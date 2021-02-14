761fc3d7d6b6f143af5367740bf52592a368d4e1
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-02-13 23:03:29 -0300

    Adicionado Logout a interface

--- lib/src/auth/repositories/auth_repository_interface.dart
+++ lib/src/auth/repositories/auth_repository_interface.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 abstract class IAuthRepository {
-  Future getUser();
-  Future getEmailPasswordLogin();
+  Future<User> getUser();
+  Future getEmailPasswordLogin(String email, String password);
   Future<String> getToken();
+  Future getLogout();
 }
