79e0db888e9302ee369f49dd22378b6d2ca0eb38
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-02-13 23:06:10 -0300

    Adicionado Login a o controller

--- lib/src/controllers/LoginPageController.dart
+++ lib/src/controllers/LoginPageController.dart
@@ -1,4 +1,6 @@
+import 'package:Brisacliente/src/auth/auth_controller.dart';
 import 'package:Brisacliente/src/models/client.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mobx/mobx.dart';
 part 'LoginPageController.g.dart';
 
@@ -6,4 +8,33 @@ class LoginPageController = _LoginPageControllerBase with _$LoginPageController;
 
 abstract class _LoginPageControllerBase with Store {
   var client = Client();
+  var authController = AuthController();
+
+  @observable
+  bool loading = false;
+
+  @observable
+  String error = "";
+
+  @action
+  setError(String value) => error = value;
+
+  @action
+  Future login(email, password) async {
+    try {
+      loading = true;
+      await authController.loginWithEmailandPassword(email, password);
+    } on FirebaseAuthException catch(e) {
+      print(e.code);
+      if (e.code == "user-not-found"){
+        setError("Email não encontrado");
+        print("Email não encontrado");
+      } 
+      if (e.code == "wrong-password"){
+        setError("CPF inválido para esse email");
+        print("CPF inválido para esse email");
+      }
+      loading = false;
+    }
+  }
 }
