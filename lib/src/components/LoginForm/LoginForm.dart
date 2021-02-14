3a7c34176ed6c70859793e89443c8fe7350e79d7
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-02-13 23:05:42 -0300

    Controller agora é passado completamente

--- lib/src/components/LoginForm/LoginForm.dart
+++ lib/src/components/LoginForm/LoginForm.dart
@@ -1,12 +1,11 @@
 import 'package:Brisacliente/src/components/LoginBoxButton/LoginBoxButton.dart';
-import 'package:Brisacliente/src/models/client.dart';
+import 'package:Brisacliente/src/controllers/LoginPageController.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
 class LoginForm extends StatefulWidget {
-  final Client client;
-  final Function cpfValidator;
-  LoginForm({Key key, this.client, this.cpfValidator}) : super(key: key);
+  final LoginPageController controller;
+  LoginForm({Key key, this.controller}) : super(key: key);
 
   @override
   _LoginFormState createState() => _LoginFormState();
@@ -23,8 +22,7 @@ class _LoginFormState extends State<LoginForm> {
     double topPadding = height * 0.03;
     return Padding(
       padding: EdgeInsets.only(
-          left: sidePadding, right: sidePadding, top: topPadding
-      ),
+          left: sidePadding, right: sidePadding, top: topPadding),
       child: Container(
         child: Form(
           key: _formKey,
@@ -36,95 +34,98 @@ class _LoginFormState extends State<LoginForm> {
                     decoration: InputDecoration(
                       labelText: "E-Mail",
                       enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(13),
-                          borderSide: BorderSide(
-                            color: Color(0xFF093d93),
-                            width: 1.2
-                          )
-                      ),
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              BorderSide(color: Color(0xFF093d93), width: 1.2)),
                       focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(13),
-                          borderSide: BorderSide(
-                            color: Color(0xFF093d93),
-                            width: 1.8
-                          )
-                      ),
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              BorderSide(color: Color(0xFF093d93), width: 1.8)),
                       errorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(13),
-                        borderSide: BorderSide(
-                          color: Color(0xFFf20505),
-                          width: 1.8
-                        )
-                      ),
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              BorderSide(color: Color(0xFFf20505), width: 1.8)),
                       focusedErrorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(13),
-                        borderSide: BorderSide(
-                          color: Color(0xFFf20505),
-                          width: 1.8
-                        )
-                      ),
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              BorderSide(color: Color(0xFFf20505), width: 1.8)),
                     ),
-                    onChanged: this.widget.client.changeEmail,
+                    onChanged: this.widget.controller.client.changeEmail
                   );
                 },
               ),
               Padding(
                 padding: EdgeInsets.only(top: topPadding),
                 child: Observer(builder: (_) {
-                  var controller = TextEditingController(text: this.widget.client.cpf);
+                  var controller = TextEditingController(
+                      text: this.widget.controller.client.cpf);
                   controller.selection = TextSelection.fromPosition(
-                    TextPosition(offset: controller.text.length)
-                  );
+                      TextPosition(offset: controller.text.length));
                   return TextFormField(
                     decoration: InputDecoration(
                       labelText: "CPF",
                       enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(13),
-                          borderSide: BorderSide(
-                            color: Color(0xFF093d93),
-                            width: 1.2
-                          )
-                      ),
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              BorderSide(color: Color(0xFF093d93), width: 1.2)),
                       focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(13),
-                          borderSide: BorderSide(
-                            color: Color(0xFF093d93),
-                            width: 1.8
-                          )
-                      ),
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              BorderSide(color: Color(0xFF093d93), width: 1.8)),
                       errorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(13),
-                        borderSide: BorderSide(
-                          color: Color(0xFFf20505),
-                          width: 1.8
-                        )
-                      ),
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              BorderSide(color: Color(0xFFf20505), width: 1.8)),
                       focusedErrorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(13),
-                        borderSide: BorderSide(
-                          color: Color(0xFFf20505),
-                          width: 1.8
-                        )
-                      ),
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              BorderSide(color: Color(0xFFf20505), width: 1.8)),
                     ),
                     controller: controller,
-                    validator: this.widget.cpfValidator,
-                    onChanged: this.widget.client.changeCpf,
+                    validator: this.widget.controller.client.validateCpf,
+                    onChanged: this.widget.controller.client.changeCpf,
                     maxLength: 14,
-                    buildCounter: (BuildContext context, { int currentLength, int maxLength, bool isFocused }) => null,
+                    buildCounter: (BuildContext context,
+                            {int currentLength,
+                            int maxLength,
+                            bool isFocused}) =>
+                        null,
                     keyboardType: TextInputType.phone,
                   );
                 }),
               ),
-              Padding(
-                padding: EdgeInsets.only(top: topPadding, bottom: topPadding),
-                child: LoginBoxButton(
-                  width: width,
-                  text: "Entrar",
-                  onPressed: () => {
-                    _formKey.currentState.validate()
-                  },
-                ),
+              Observer(
+                builder: (_){
+                return Padding(
+                    padding: EdgeInsets.only(top: topPadding, bottom: topPadding),
+                    child: LoginBoxButton(
+                      width: width,
+                      text: "Entrar",
+                      onPressed: () {
+                        if (_formKey.currentState.validate()){
+                          this.widget.controller.login(
+                            this.widget.controller.client.email,
+                            this.widget.controller.client.cpf
+                          );
+                        }
+                      },
+                      isEnabled: !this.widget.controller.loading,
+                    ),
+                  );
+                },
+              ),
+              Observer(
+                builder: (_){
+                  return SizedBox(
+                    height: this.widget.controller.error.length > 0 ? height * 0.05 : 0,
+                    child: Text(
+                      this.widget.controller.error,
+                      style: TextStyle(
+                        color: Colors.red
+                        ),
+                      )
+                  );
+                }
               )
             ],
           ),
