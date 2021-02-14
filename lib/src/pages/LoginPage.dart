748d4d056f99175a3f74363130f08925f66abafe
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-02-13 23:07:24 -0300

    Adicionado Scaffold e rotas nomeadas

--- lib/src/pages/LoginPage.dart
+++ lib/src/pages/LoginPage.dart
@@ -21,105 +21,97 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-    return Stack(
-        children: [
-          Positioned(
-            top: 0,
-            child: Container(
-              color: Color(0xFF093d93),
-              height: height * 0.33,
+    return Scaffold(
+        backgroundColor: Color(0xFFecf0f1),
+        body: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              child: Container(
+                color: Color(0xFF093d93),
+                height: height * 0.33,
+                width: width,
+              ),
+            ),
+            Positioned(
+              top: 100,
+              height: height,
               width: width,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: height * 0.08,
+                      child: Text(
+                        "Brisacliente", 
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500)
+                        )
+                    ),
+                    LoginBox(
+                      type: "column",
+                      children: [
+                        LoginForm(
+                          controller: this.controller
+                        )
+                      ],
+                    ),
+                    LoginBox(
+                      height: height * 0.125,
+                      radius: 13,
+                      children: [
+                        SizedBox(
+                          width: width / 1.7,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              LoginBoxTitle(text: "ACESSO COMO VISITANTE"),
+                              LoginBoxText(text: "Assine seus contratos, envie suas fotos e acompanhe sua instação!")
+                            ],
+                          ),
+                        ),
+                        LoginBoxButton(
+                          text: "ACESSAR",
+                          onPressed: (){
+                            Navigator.pushNamed(context, "/guestpage");
+                          }
+                        )
+                      ],
+                    ),
+                    LoginBox(
+                      height: height * 0.125,
+                      radius: 13,
+                      children: [
+                        SizedBox(
+                          width: width / 1.7,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              LoginBoxTitle(text: "PRIMEIRO ACESSO / RECUPERAR SENHA"),
+                              LoginBoxText(text: "Primeira vez por aqui? Ou esqueceu a senha?"),
+                            ],
+                          ),
+                        ),
+                        LoginBoxButton(
+                          text: "ACESSAR",
+                          isInverted: true,
+                          onPressed: (){
+                            Navigator.pushNamed(context, "/firstaccess");
+                          }
+                        )
+                      ],
+                    ),
+                  ],
+                crossAxisAlignment: CrossAxisAlignment.center,
+              ),
             ),
           ),
-          Positioned(
-            top: 100,
-            height: height,
-            width: width,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: height * 0.08,
-                    child: Text(
-                      "Brisacliente", 
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500)
-                      )
-                  ),
-                  LoginBox(
-                    type: "column",
-                    children: [
-                      LoginForm(
-                        client: this.controller.client,
-                        cpfValidator: this.controller.client.validateCpf
-                      )
-                    ],
-                  ),
-                  LoginBox(
-                    height: height * 0.125,
-                    radius: 13,
-                    children: [
-                      SizedBox(
-                        width: width / 1.7,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            LoginBoxTitle(text: "ACESSO COMO VISITANTE"),
-                            LoginBoxText(text: "Assine seus contratos, envie suas fotos e acompanhe sua instação!")
-                          ],
-                        ),
-                      ),
-                      LoginBoxButton(
-                        text: "ACESSAR",
-                        onPressed: (){
-                           Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => GuestPage()
-                            ),
-                          );
-                        }
-                      )
-                    ],
-                  ),
-                  LoginBox(
-                    height: height * 0.125,
-                    radius: 13,
-                    children: [
-                      SizedBox(
-                        width: width / 1.7,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            LoginBoxTitle(text: "PRIMEIRO ACESSO / RECUPERAR SENHA"),
-                            LoginBoxText(text: "Primeira vez por aqui? Ou esqueceu a senha?"),
-                          ],
-                        ),
-                      ),
-                      LoginBoxButton(
-                        text: "ACESSAR",
-                        isInverted: true,
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => FirstAcessPage()
-                            ),
-                          );
-                        }
-                      )
-                    ],
-                  ),
-                ],
-              crossAxisAlignment: CrossAxisAlignment.center,
-            ),
-          ),
-        ),
-      ]
+        ]
+      ),
     );
   }
 }
