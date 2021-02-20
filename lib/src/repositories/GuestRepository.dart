21a21ec59bdbb4afa8d0e9775c8ac1c1dd02be9f
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-02-20 17:25:43 -0300

    GuestRepository criado e implementado

---
+++ lib/src/repositories/GuestRepository.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+import 'package:Brisacliente/src/models/contract.dart';
+import 'package:Brisacliente/src/repositories/interfaces/GuestRepository_Interface.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class GuestRepository implements IGuestRepository{
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  List<Contract> contracts = [];
+  Map<String, dynamic> data;
+  String id;
+
+  @override
+  void acceptContract(int index) {
+    contracts[index].setIsAccepted(true);
+  }
+
+  @override
+  Future<List<Contract>> getContracts() async {
+    contracts = [];
+    for (DocumentReference contractReference in data["contracts"]){
+      var doc = await firestore.collection("Contracts").doc(contractReference.id).get();
+      String title = doc.data()["title"];
+      String type = doc.data()["type"];
+      var contract = Contract(title, type);
+
+      contracts.add(contract);
+    }
+
+    return contracts;
+  }
+
+  @override
+  Future<bool> isContractsNotAccepted(String cpf) async {
+    print("Contratos não aceitos");
+    var docs = await firestore.collection("ContractsNotAccepted")
+      .where("cpf",isEqualTo:  cpf).limit(1).get();
+    if (docs.docs.isEmpty){
+      return false;
+    }
+
+    var doc = docs.docs[0];
+    data = doc.data();
+    id = doc.id;
+
+    return true;
+  }
+
+  @override
+  Future<Uri> sendDocuments(List<File> images) {
+    throw UnimplementedError();
+  }
+
+}
