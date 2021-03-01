c2f712c2d8d0dc208c9b80ea3ed2eeea53de658e
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2021-03-01 12:54:53 -0300

    Interface atualizada

--- lib/src/repositories/interfaces/GuestRepository_Interface.dart
+++ lib/src/repositories/interfaces/GuestRepository_Interface.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 import 'package:Brisacliente/src/models/contract.dart';
+import 'package:mobx/mobx.dart';
 
 abstract class IGuestRepository {
   Future<bool> isContractsNotAccepted(String cpf);
-  Future<Uri> sendDocuments(List<File> images);
+  Future sendDocuments(ObservableMap<String, File> images, String cpf);
   Future<List<Contract>> getContracts();
   void acceptContract(int index);
 }
