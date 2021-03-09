c481a64e0c74263a52832e80538794bca4f3c937
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-03-09 13:05:34 -0300

    Adicionado redirecionamento

--- lib/src/controllers/LoginPageController.dart
+++ lib/src/controllers/LoginPageController.dart
@@ -1,6 +1,7 @@
 import 'package:Brisacliente/src/auth/auth_controller.dart';
 import 'package:Brisacliente/src/models/client.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 part 'LoginPageController.g.dart';
 
@@ -8,7 +9,7 @@ class LoginPageController = _LoginPageControllerBase with _$LoginPageController;
 
 abstract class _LoginPageControllerBase with Store {
   var client = Client();
-  var authController = AuthController();
+  var _authController = AuthController();
 
   @observable
   bool loading = false;
@@ -20,21 +21,24 @@ abstract class _LoginPageControllerBase with Store {
   setError(String value) => error = value;
 
   @action
-  Future login(email, password) async {
+  Future login(email, password, BuildContext context) async {
     try {
       loading = true;
-      await authController.loginWithEmailandPassword(email, password);
-    } on FirebaseAuthException catch(e) {
-      print(e.code);
-      if (e.code == "user-not-found"){
+      await _authController.loginWithEmailandPassword(email, password);
+      Navigator.popAndPushNamed(
+        context, 
+        "/home", 
+        arguments: _authController
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
         setError("Email não encontrado");
-        print("Email não encontrado");
-      } 
-      if (e.code == "wrong-password"){
+      }
+      if (e.code == "wrong-password") {
         setError("CPF inválido para esse email");
-        print("CPF inválido para esse email");
       }
       loading = false;
     }
+    loading = false;
   }
 }
