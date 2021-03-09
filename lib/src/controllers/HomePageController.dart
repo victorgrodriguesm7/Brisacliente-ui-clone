2b7784416c61f5ae71925eabb624a50ab472a4c4
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-03-09 13:13:05 -0300

    HomePageController criado

---
+++ lib/src/controllers/HomePageController.dart
@@ -0,0 +1,42 @@
+import 'package:Brisacliente/src/auth/auth_controller.dart';
+import 'package:Brisacliente/src/repositories/PlansRepository.dart';
+import 'package:Brisacliente/src/models/plan.dart';
+import 'package:mobx/mobx.dart';
+part 'HomePageController.g.dart';
+
+class HomePageController extends _HomePageControllerBase
+    with _$HomePageController {
+  HomePageController(AuthController authController) {
+    super._authController = authController;
+    super._plansRepository = PlansRepository(_authController.user.uid);
+    super.getAddress();
+    super.getPlans();
+  }
+}
+
+abstract class _HomePageControllerBase with Store {
+  AuthController _authController;
+  PlansRepository _plansRepository;
+
+  @observable
+  int index = 0;
+
+  @action
+  setIndex(int value) => index = value;
+
+  getUserName() => _authController.user.displayName;
+
+  @observable
+  String address;
+
+  @action
+  Future<void> getAddress() async =>
+      address = await _plansRepository.getAddress();
+
+  @observable
+  List<Plan> plans;
+
+  @action
+  Future<List<Plan>> getPlans() async =>
+      plans = await _plansRepository.getPlans();
+}
